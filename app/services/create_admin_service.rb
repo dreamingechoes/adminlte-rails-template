7e5b73011c41e8235721b83b25895a6378073d87
Ivan Gonzalez <hi@dreamingecho.es>
2016-12-22 16:34:47 +0100

    Fix create_admin_service adding user name on create

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,6 +1,7 @@
 class CreateAdminService
   def call
-    User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
+    User.find_or_create_by!(email: Rails.application.secrets.admin_email,
+                            name: Rails.application.secrets.admin_name) do |user|
       user.password = Rails.application.secrets.admin_password
       user.password_confirmation = Rails.application.secrets.admin_password
       user.admin!
